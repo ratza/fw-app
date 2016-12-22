1d9eb20407dce407e0af4e40c6f7902ea021534b
kokyhin <kokyhin@gmail.com>
2016-12-22 18:47:09 +0200

    Added posibility to include SCSS file and autoprefixe them

--- fw-app-front/src/App.js
+++ fw-app-front/src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header logo={''}>
+        <Header logo={logo}>
           <h1>Hello lady!</h1>
         </Header>
       </div>
